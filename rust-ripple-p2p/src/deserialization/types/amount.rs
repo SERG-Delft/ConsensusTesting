ee0e0fd6defd5846322d1a71f7780e4186251420
Florena Buse <i.buse@student.tudelft.nl>
2022-03-26 12:25:40 +0100

    Fix bug for amount

--- rust-ripple-p2p/src/deserialization/types/amount.rs
+++ rust-ripple-p2p/src/deserialization/types/amount.rs
@@ -12,7 +12,7 @@ impl Amount {
     pub fn parse(blob: &mut BlobIterator) -> Self {
         let is_xrp: bool = (blob.peek() & 0b1000_0000) == 0;
         if !is_xrp { panic!("cannot parse issued currency") }
-        Amount { amount: u64::from_be_bytes(blob.next_n_bytes(8).try_into().unwrap()) }
+        Amount { amount: (u64::from_be_bytes(blob.next_n_bytes(8).try_into().unwrap())) ^ 0x4000000000000000 }
     }
 }
 
