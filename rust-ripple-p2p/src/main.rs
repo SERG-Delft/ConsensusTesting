94b254d082f0fde11d363dd689a82ccc8fee0830
Levin Winter <33220502+levinwinter@users.noreply.github.com>
2022-07-03 14:58:29 +0200

    setup second experiment

--- rust-ripple-p2p/src/main.rs
+++ rust-ripple-p2p/src/main.rs
@@ -86,7 +86,7 @@ fn main() {
     let mut file = fs::OpenOptions::new()
         .write(true)
         .append(true) // This is needed to append to file
-        .open("results-buggy-7-2-0-6.txt")
+        .open("results-buggy-7-3-0-6.txt")
         .unwrap();
 
     loop {
