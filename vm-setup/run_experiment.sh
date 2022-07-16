0cf0f3bc13e353c5366b7c3fb9b7eca967ca7393
Martijn van Meerten <Martijn.vanmeerten@vixion.nl>
2022-07-17 01:19:21 +0200

    Fix experiment script

--- vm-setup/run_experiment.sh
+++ vm-setup/run_experiment.sh
@@ -5,6 +5,5 @@ result_folder_name=$3
 
 for ((i=1;i<=$n;i++));
 do
-	echo $config $result_folder_name$i
-	#/root/snap/lxd/current/ConsensusTesting/rust-ripple-p2p/target/release/rust-ripple-p2p $config $result_folder_name$i
+	/root/snap/lxd/current/ConsensusTesting/rust-ripple-p2p/target/release/rust-ripple-p2p $config $result_folder_name$i
 done
