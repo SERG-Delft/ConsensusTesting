294e8256a1bc868da255e91f9bf381122a1a1177
Levin Winter <33220502+levinwinter@users.noreply.github.com>
2022-10-20 16:22:52 +0200

    Add Insufficient Support

--- rust-ripple-p2p/analyze.py
+++ rust-ripple-p2p/analyze.py
@@ -11,7 +11,14 @@ if len(sys.argv) == 3:
 
 uncategorized_count = 0
 
-print('          |TOTL|CORR|INCO|UNKN')
+def neg(x):
+    return lambda x: not x
+
+def insufficient_support_l(line: str):
+    return bool(re.search('[flag] Timeout after \d+ messages\n', line)) or \
+        bool(re.search('[flag] Ledger \d+ diverged and has insufficient support\n', line))
+
+print('          |TOTAL|CORRECT|INSUFFICIENT|INCOMPLETE|UNCATEGORIZED')
 
 for config in os.listdir('traces'):
     (c, d, scope) = re.search('buggy-7-(\d)-(\d)-6-(.*)-0\.2\.4', config).groups()
@@ -25,18 +32,27 @@ for config in os.listdir('traces'):
     correct = []
     incomplete = []
     uncategorized = []
+    insufficient_support = []
     for run in runs:
         results = open('traces/' + config + '/' + run + '/results.txt').readlines()
         if results[-1] != 'done!\n':
             incomplete.append(run)
         elif results[-2] == 'reason: all committed\n':
             correct.append(run)
+        elif results[4] == 'reason: flags\n':
+            flags = results[5:-1]
+            if len(list(filter(neg(insufficient_support_l), flags))) == 0:
+                insufficient_support.append(run)
+            else:
+                uncategorized.append(run)
+                uncategorized_count += 1
         else:
             uncategorized.append(run)
             uncategorized_count += 1
-    print(f'{len(runs):4d}', end = '|')
-    print(f'{len(correct):4d}', end = '|')
-    print(f'{len(incomplete):4d}', end = '|')
-    print(f'{len(uncategorized):4d}', uncategorized)
+    print(f'{len(runs):5d}', end = '|')
+    print(f'{len(correct):7d}', end = '|')
+    print(f'{len(insufficient_support):12d}', end = '|')
+    print(f'{len(incomplete):10d}', end = '|')
+    print(f'{len(uncategorized):13d}', uncategorized)
 
 print(uncategorized_count, 'uncategorized')
