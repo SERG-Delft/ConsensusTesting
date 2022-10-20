7e0d127174ee234167d2dbf71a810ca14b8328d7
Levin Winter <33220502+levinwinter@users.noreply.github.com>
2022-10-20 19:01:11 +0200

    Remove Loop

--- rust-ripple-p2p/src/main.rs
+++ rust-ripple-p2p/src/main.rs
@@ -84,158 +84,154 @@ fn main() {
         (7, 1, 2, 6, false, false),
         (7, 1, 2, 6, true, false),
     ];
+    for (_, c, d, r, any, base) in configs {
+        let args = Args {
+            n,
+            c,
+            d,
+            r,
+            any_scope: any,
+            baseline: base,
+            toxiproxy_path: args.toxiproxy_path.clone(),
+        };
+        let bug_unls: Vec<Vec<usize>> = vec![
+            // symmetric
+            vec![0, 1, 2, 3, 4], // 0
+            vec![0, 1, 2, 3, 4], // 1
+            vec![0, 1, 2, 3, 4], // 2
+            vec![0, 1, 2, 3, 4], // 3
+            vec![2, 3, 4, 5, 6], // 4
+            vec![2, 3, 4, 5, 6], // 5
+            vec![2, 3, 4, 5, 6], // 6
 
-    loop {
-        for (_, c, d, r, any, base) in configs {
-            let args = Args {
-                n,
-                c,
-                d,
-                r,
-                any_scope: any,
-                baseline: base,
-                toxiproxy_path: args.toxiproxy_path.clone(),
-            };
-            let bug_unls: Vec<Vec<usize>> = vec![
-                // symmetric
-                vec![0, 1, 2, 3, 4], // 0
-                vec![0, 1, 2, 3, 4], // 1
-                vec![0, 1, 2, 3, 4], // 2
-                vec![0, 1, 2, 3, 4], // 3
-                vec![2, 3, 4, 5, 6], // 4
-                vec![2, 3, 4, 5, 6], // 5
-                vec![2, 3, 4, 5, 6], // 6
+                                 // vec![0, 1, 2, 3, 4, 5], // 0
+                                 // vec![0, 1, 2, 3, 4, 5], // 1
+                                 // vec![0, 1, 2, 3, 4, 5], // 2
+                                 // vec![0, 1, 2, 3, 4, 5], // 3
+                                 // vec![1, 2, 3, 4, 5, 6], // 4
+                                 // vec![1, 2, 3, 4, 5, 6], // 5
+                                 // vec![1, 2, 3, 4, 5, 6], // 6
 
-                                     // vec![0, 1, 2, 3, 4, 5], // 0
-                                     // vec![0, 1, 2, 3, 4, 5], // 1
-                                     // vec![0, 1, 2, 3, 4, 5], // 2
-                                     // vec![0, 1, 2, 3, 4, 5], // 3
-                                     // vec![1, 2, 3, 4, 5, 6], // 4
-                                     // vec![1, 2, 3, 4, 5, 6], // 5
-                                     // vec![1, 2, 3, 4, 5, 6], // 6
+                                 // vec![0, 1, 2, 3, 4, 5, 6], // 0
+                                 // vec![0, 1, 2, 3, 4, 5, 6], // 1
+                                 // vec![0, 1, 2, 3, 4, 5, 6], // 2
+                                 // vec![0, 1, 2, 3, 4, 5, 6], // 3
+                                 // vec![0, 1, 2, 3, 4, 5, 6], // 4
+                                 // vec![0, 1, 2, 3, 4, 5, 6], // 5
+                                 // vec![0, 1, 2, 3, 4, 5, 6], // 6
 
-                                     // vec![0, 1, 2, 3, 4, 5, 6], // 0
-                                     // vec![0, 1, 2, 3, 4, 5, 6], // 1
-                                     // vec![0, 1, 2, 3, 4, 5, 6], // 2
-                                     // vec![0, 1, 2, 3, 4, 5, 6], // 3
-                                     // vec![0, 1, 2, 3, 4, 5, 6], // 4
-                                     // vec![0, 1, 2, 3, 4, 5, 6], // 5
-                                     // vec![0, 1, 2, 3, 4, 5, 6], // 6
+                                 // // config 1.5
+                                 // vec![0, 1, 2, 3, 7], // 0
+                                 // vec![0, 1, 2, 3, 7], // 1
+                                 // vec![0, 1, 2, 3, 7], // 2
+                                 // vec![0, 1, 2, 3, 4, 5, 6, 7, 8], // 3
+                                 // vec![3, 4, 5, 6, 8], // 4
+                                 // vec![3, 4, 5, 6, 8], // 5
+                                 // vec![3, 4, 5, 6, 8], // 6
+                                 // vec![0, 1, 2, 3, 7], // 7
+                                 // vec![3, 4, 5, 6, 8], // 8
 
-                                     // // config 1.5
-                                     // vec![0, 1, 2, 3, 7], // 0
-                                     // vec![0, 1, 2, 3, 7], // 1
-                                     // vec![0, 1, 2, 3, 7], // 2
-                                     // vec![0, 1, 2, 3, 4, 5, 6, 7, 8], // 3
-                                     // vec![3, 4, 5, 6, 8], // 4
-                                     // vec![3, 4, 5, 6, 8], // 5
-                                     // vec![3, 4, 5, 6, 8], // 6
-                                     // vec![0, 1, 2, 3, 7], // 7
-                                     // vec![3, 4, 5, 6, 8], // 8
+                                 // config 2
+                                 // vec![0, 1, 2, 3, 7, 8], // 0
+                                 // vec![0, 1, 2, 3, 7, 8], // 1
+                                 // vec![0, 1, 2, 3, 7, 8], // 2
+                                 // vec![0, 1, 2, 3, 4, 5, 6, 7, 8], // 3
+                                 // vec![3, 4, 5, 6, 7, 8], // 4
+                                 // vec![3, 4, 5, 6, 7, 8], // 5
+                                 // vec![3, 4, 5, 6, 7, 8], // 6
+                                 // vec![0, 1, 2, 3, 4, 5, 6, 7, 8], // 7
+                                 // vec![0, 1, 2, 3, 4, 5, 6, 7, 8], // 8
+        ];
 
-                                     // config 2
-                                     // vec![0, 1, 2, 3, 7, 8], // 0
-                                     // vec![0, 1, 2, 3, 7, 8], // 1
-                                     // vec![0, 1, 2, 3, 7, 8], // 2
-                                     // vec![0, 1, 2, 3, 4, 5, 6, 7, 8], // 3
-                                     // vec![3, 4, 5, 6, 7, 8], // 4
-                                     // vec![3, 4, 5, 6, 7, 8], // 5
-                                     // vec![3, 4, 5, 6, 7, 8], // 6
-                                     // vec![0, 1, 2, 3, 4, 5, 6, 7, 8], // 7
-                                     // vec![0, 1, 2, 3, 4, 5, 6, 7, 8], // 8
-            ];
+        let execution_string = format!(
+            "buggy-{}-{}-{}-{}-{}-{}",
+            args.n,
+            args.c,
+            args.d,
+            args.r,
+            if args.baseline {
+                "baseline"
+            } else if args.any_scope {
+                "any-scope"
+            } else {
+                "small-scope"
+            },
+            crate_version!()
+        );
 
-            let execution_string = format!(
-                "buggy-{}-{}-{}-{}-{}-{}",
+        let mut file = fs::File::create("results.txt").unwrap();
+
+        {
+            let runtime = tokio::runtime::Runtime::new().unwrap();
+            let time = SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_secs();
+
+            let (flags_tx, mut flags_rx) = broadcast::channel(32);
+            let flag_collector = runtime.spawn(async move {
+                let mut flags = Vec::new();
+                while let Ok(message) = flags_rx.recv().await {
+                    flags.push(message);
+                }
+                flags
+            });
+
+            let (shutdown_tx, shutdown_rx) = broadcast::channel(16);
+            let mut results = shutdown_rx.resubscribe();
+
+            let mut toxiproxy = Command::new(toxiproxypath)
+                .stderr(Stdio::null())
+                .spawn()
+                .unwrap();
+            let node_keys = start_docker_containers(n, &bug_unls);
+            for k in &node_keys {
+                println!("node key {}", k.validation_public_key);
+            }
+
+            let byzz_fuzz = ByzzFuzz::new(
                 args.n,
                 args.c,
                 args.d,
                 args.r,
-                if args.baseline {
-                    "baseline"
-                } else if args.any_scope {
-                    "any-scope"
-                } else {
-                    "small-scope"
-                },
-                crate_version!()
+                args.any_scope,
+                args.baseline,
+                node_keys.clone(),
             );
+            println!("{:?}", &byzz_fuzz.process_faults);
+            file.write_fmt(format_args!(
+                "process faults {:?}\n",
+                &byzz_fuzz.process_faults
+            ))
+            .expect("could not log byzzfuzz");
+            file.write_fmt(format_args!(
+                "network faults {:?}\n",
+                &byzz_fuzz.network_faults
+            ))
+            .expect("could not log byzzfuzz");
+            let app = app::App::new(n as u16, node_keys);
 
-            let mut file = fs::File::create("results.txt").unwrap();
-
-            {
-                let runtime = tokio::runtime::Runtime::new().unwrap();
-                let time = SystemTime::now()
-                    .duration_since(UNIX_EPOCH)
-                    .unwrap()
-                    .as_secs();
-
-                let (flags_tx, mut flags_rx) = broadcast::channel(32);
-                let flag_collector = runtime.spawn(async move {
-                    let mut flags = Vec::new();
-                    while let Ok(message) = flags_rx.recv().await {
-                        flags.push(message);
-                    }
-                    flags
-                });
-
-                let (shutdown_tx, shutdown_rx) = broadcast::channel(16);
-                let mut results = shutdown_rx.resubscribe();
-
-                let mut toxiproxy = Command::new(toxiproxypath)
-                    .stderr(Stdio::null())
-                    .spawn()
-                    .unwrap();
-                let node_keys = start_docker_containers(n, &bug_unls);
-                for k in &node_keys {
-                    println!("node key {}", k.validation_public_key);
-                }
-
-                let byzz_fuzz = ByzzFuzz::new(
-                    args.n,
-                    args.c,
-                    args.d,
-                    args.r,
-                    args.any_scope,
-                    args.baseline,
-                    node_keys.clone(),
-                );
-                println!("{:?}", &byzz_fuzz.process_faults);
-                file.write_fmt(format_args!(
-                    "process faults {:?}\n",
-                    &byzz_fuzz.process_faults
-                ))
-                .expect("could not log byzzfuzz");
-                file.write_fmt(format_args!(
-                    "network faults {:?}\n",
-                    &byzz_fuzz.network_faults
-                ))
-                .expect("could not log byzzfuzz");
-                let app = app::App::new(n as u16, node_keys);
-
-                if let Err(error) = runtime.block_on(app.start(byzz_fuzz, shutdown_tx, flags_tx)) {
-                    error!("Error: {}", error);
-                }
-
-                // let check = Command::new("node").arg(r"C:\Users\levin\git\xrp\index.js").output().unwrap();
-                let (map, agreed, reason) =
-                    runtime.block_on(async { results.recv().await.unwrap() });
-                file.write_all(format!("{:?}\n{:?}\nreason: {}\n", map, agreed, reason).as_bytes())
-                    .expect("could not write");
-
-                let flags = runtime.block_on(flag_collector).unwrap();
-                for flag in flags {
-                    file.write_all(format!("[flag] {}\n", flag).as_bytes())
-                        .unwrap();
-                }
-
-                file.write_all("done!\n".as_bytes()).unwrap();
-
-                runtime.block_on(save_results(&execution_string, time));
-
-                toxiproxy.kill().unwrap();
-                runtime.shutdown_timeout(Duration::from_millis(100));
+            if let Err(error) = runtime.block_on(app.start(byzz_fuzz, shutdown_tx, flags_tx)) {
+                error!("Error: {}", error);
             }
+
+            // let check = Command::new("node").arg(r"C:\Users\levin\git\xrp\index.js").output().unwrap();
+            let (map, agreed, reason) = runtime.block_on(async { results.recv().await.unwrap() });
+            file.write_all(format!("{:?}\n{:?}\nreason: {}\n", map, agreed, reason).as_bytes())
+                .expect("could not write");
+
+            let flags = runtime.block_on(flag_collector).unwrap();
+            for flag in flags {
+                file.write_all(format!("[flag] {}\n", flag).as_bytes())
+                    .unwrap();
+            }
+
+            file.write_all("done!\n".as_bytes()).unwrap();
+
+            runtime.block_on(save_results(&execution_string, time));
+
+            toxiproxy.kill().unwrap();
+            runtime.shutdown_timeout(Duration::from_millis(100));
         }
     }
 }
