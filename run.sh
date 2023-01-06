bb0e5cceb43f4eeb2e9f2451fade1d40bc16663b
Levin Winter <33220502+levinwinter@users.noreply.github.com>
2023-01-06 17:40:56 +0000

    Ignore if container is missing

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ n=${1:-300}
 for ((i=0; i<$n; i++))
 do
     timeout --foreground 15m \
-    docker rm byzzfuzz && \
+    docker rm byzzfuzz || true && \
     docker run -v /var/run/docker.sock:/var/run/docker.sock -i --init --net host --name byzzfuzz byzzfuzz && \
     docker cp byzzfuzz:/home/traces . && \
     docker rm byzzfuzz
