2c96459e1d4863eec59eb27e6c49b710d7b720ff
Levin Winter <33220502+levinwinter@users.noreply.github.com>
2023-01-06 17:59:29 +0000

    More Time

--- run.sh
+++ run.sh
@@ -4,9 +4,8 @@ n=${1:-300}
 
 for ((i=0; i<$n; i++))
 do
-    timeout --foreground 15m \
     docker rm byzzfuzz || true && \
-    docker run -v /var/run/docker.sock:/var/run/docker.sock -i --init --net host --name byzzfuzz byzzfuzz && \
+    timeout --foreground 25m docker run -v /var/run/docker.sock:/var/run/docker.sock -i --init --net host --name byzzfuzz byzzfuzz && \
     docker cp byzzfuzz:/home/traces . && \
     docker rm byzzfuzz
 done
